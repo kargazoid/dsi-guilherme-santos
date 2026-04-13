f884f402cc2ae95b7100ca0709c137643294c6b3
Guilherme Santos <guilhermemigueldossantos2010@gmail.com>
2026-04-13 09:20:18 -0300

    Aula 04 feita sobre tipos de variáveis

---
+++ aula04/Exemplosvariaveis.java
@@ -0,0 +1,22 @@
+package aula04;
+
+public class Exemplosvariaveis {
+
+    public static void main(String[] args) {
+        String string = "Esta é uma string ";
+        int inte = 22;
+        boolean bool = true;
+        double doub = 3.14;
+        long longo = 801964361980452143L;
+        char caractere = '♀';
+        short pequeno = 10;
+
+        System.out.println(string);
+        System.out.println(bool);
+        System.out.println(caractere);
+        System.out.println(inte);
+        System.out.println(doub);
+        System.out.println(longo);
+        System.out.println(pequeno);
+    }
+}
